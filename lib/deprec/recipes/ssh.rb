d9596681f8ee191677bedfe980c7b52aaa58cf97
Le1t0 <dev@ewout.to>
2010-07-14 11:43:50 +0200

    add to known hosts for IP addresses as well as hostnames for SSH recipe

--- lib/deprec/recipes/ssh.rb
+++ lib/deprec/recipes/ssh.rb
@@ -68,9 +68,12 @@ Capistrano::Configuration.instance(:must_exist).load do
           run "mv ~/.ssh/authorized_keys.new ~/.ssh/authorized_keys"
         end
 
+        require 'socket'
         if ssh_known_hosts.size > 0
-          put ssh_known_hosts.join("\n"), tmp_file = "/tmp/ssh_keyscan_#{Time.now.strftime("%Y%m%d%H%M%S")}.txt", :mode => 0644
-          run "ssh-keyscan -f #{tmp_file} -t rsa > ~/.ssh/known_hosts ; rm -f #{tmp_file}"
+          hosts_and_ips = (ssh_known_hosts + ssh_known_hosts.collect { |h| Socket.getaddrinfo(h, 0, Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP, Socket::INADDR_ANY).first[3] })
+          put (hosts_and_ips + [""]).join("\n"), tmp_file = "/tmp/ssh_keyscan_#{Time.now.strftime("%Y%m%d%H%M%S")}.txt", :mode => 0644
+          run "ssh-keyscan -f #{tmp_file} -t rsa > ~/.ssh/known_hosts"
+          run "rm -f #{tmp_file}"
         end
       end
       
